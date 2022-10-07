5027354a96e36760b0dae29c8c86d4b909be7408
thopatevijay <thopatevijay@gmail.com>
2022-10-07 20:42:58 +0530

    lesson 9 completed

--- contracts/MyERC20.sol
+++ contracts/MyERC20.sol
@@ -4,5 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyERC20Token is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {}
+    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
 } 
